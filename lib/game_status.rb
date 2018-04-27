eb4671c70c87ecb29cd145718a0cc6bb19e1ee39
Nicolas Marini <nmarini44@gmail.com>
2018-04-27 22:02:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,6 +36,9 @@ def draw?(board)
 end
 
 def over?(board)
-  won?(board) && full?(board) && draw?(board)
-    end
+  if won?(board) && full?(board) && draw?(board)
+    return true
+  else
+    return false
+  end
 end
