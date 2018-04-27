0dc970098929977d3801e44cfb18d27547282ae3
Nicolas Marini <nmarini44@gmail.com>
2018-04-27 20:41:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,12 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.find do |win_combo|
+    if position_taken?(board, win_combo[0]) == true && position_taken?(board, win_combo[1]) == true && position_taken?(board, win_combo[2]) == true &&
+      win_combo[0] == "X" && win_combo[1] == "X" && win_combo[2] == "X" || position_taken?(board, win_combo[0]) == true && position_taken?(board, win_combo[1]) == true && position_taken?(board, win_combo[2]) == true &&
+      win_combo[0] == "O" && win_combo[1] == "O" && win_combo[2] == "O"
+      return win_combo
+    end
+  end
