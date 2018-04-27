2f1fd450954f21c2bf73aae40bea826b661ba13f
Nicolas Marini <nmarini44@gmail.com>
2018-04-27 20:55:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,8 +27,8 @@ def won?(board)
 
 if position_1 == "X" && position_2 == "X" && position_3 == "X"|| position_1 == "O" && position_2 == "O" && position_3 == "O"
    return win_combo
- else
-  return false
+ else  
+  false
 end
 end
 end
